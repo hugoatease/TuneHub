d863a3cccd5d131c02aeaebeb927002126e9b517
Hugo Caille <hugo@gkz.fr.nf>
2011-06-10 21:54:41 +0200

    Added compatibility with Mutagen in ID3Handler

--- modules/id3handler.py
+++ modules/id3handler.py
@@ -7,11 +7,124 @@ from os import system
 import datastruct
 
 class ID3handler:
+	
+	def __init__(self, filename):
+		self.filename = filename
+		self.motorname = 'classic'
+		
+	def filter(self):
+		ismp3 = '.mp3' in self.filename
+		if ismp3 == False:
+			self.motor = 'classic'
+			return True
+		else:
+			return False
+		
+	def isMetagen(self):
+		mutagenerror = False
+		try:
+			import mutagen
+		except:
+			mutagenerror = True
+		
+		if mutagenerror == False:
+			self.motorname = 'mutagen'
+			
+	def initMotor(self):
+		motorname = self.motorname
+		
+		if motorname == 'classic':
+			motor = ClassicParsers(self.filename)
+			self.motor = motor
+		if motorname == 'mutagen':
+			motor = Mutagen(self.filename)
+			self.motor = motor
+			
+	def get(self):
+		self.isMetagen()
+		self.filter()
+		self.initMotor()
+		motor = self.motor
+		dic = motor.get()
+		return dic
+				
+class Mutagen:
+	
+	def getFormat(self):
+		filename = self.filename
+		if '.mp3' in filename:
+			result = 'mp3'
+		elif '.m4a' in filename:
+			result = 'aac'
+		elif '.ogg' in filename:
+			result = 'vorbis'
+		else:
+			result = None
+		
+		return result
+	
+	def mutagenInit(self):
+		format = self.format
+		
+		if format == 'mp3':
+			import mutagen.mp3
+			self.id3api = mutagen.mp3
+		if format == 'aac':
+			import mutagen.m4a
+			self.id3api = mutagen.m4a
+		if format == 'vorbis':
+			import mutagen.oggvorbis
+			self.id3api = mutagen.oggvorbis
+	
+	def __init__(self, filename):
+		self.filename = filename
+		self.id3api = None
+		self.format = self.getFormat()
+		self.mutagenInit()
+		
+	def getDataTuple(self):
+		try:
+			self.mutagenData = self.id3api.Open(self.filename)
+		except self.mutagenData.ID3Warning:
+			pass
+		
+	def get(self):
+		structAPI = datastruct.Structure()
+		self.getDataTuple()
+		datatuple = self.mutagenData
+		
+		if self.mutagenData != None:
+			
+			try:
+				album = datatuple['TALB'][0]
+			except:
+				album = None
+			try:
+				title = datatuple['TIT2'][0]
+			except:
+				title = None
+			try:
+				artist = datatuple['TPE1'][0]
+			except:
+				artist = None
+			
+			structAPI.Artist(artist)
+			structAPI.Album(album)
+			structAPI.Title(title)
+			structAPI.Filename(self.filename)
+			
+			structure = structAPI.get()
+			return structure
+		else:
+			return 0
+		
+		
+class ClassicParsers:
 
 	def __init__(self, filename):
 		self.filename = filename
 
-	def basicInfov2(self):
+	def getV2(self):
 		mp3= self.filename
 		try:
 			id3 = ID3v2.ID3v2(filename=mp3)
@@ -52,7 +165,7 @@ class ID3handler:
 		else:
 			return 0
 
-	def basicInfov1(self):
+	def getV1(self):
 		structAPI = datastruct.Structure()
 		mp3 = self.filename
 		try:
@@ -70,10 +183,10 @@ class ID3handler:
 		else:
 			return 0
 
-	def basicInfo(self):
-		dic = self.basicInfov1()
+	def get(self):
+		dic = self.getV1()
 		if dic == 0:
-			dic = self.basicInfov2()
+			dic = self.getV2()
 		return dic
 
 	def writeLyrics(self,lyrics):
