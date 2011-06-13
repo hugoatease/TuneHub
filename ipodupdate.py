ac5730285e9b3f11ae39d18495a3b5717823b873
Hugo Caille <hugo@gkz.fr.nf>
2011-06-13 21:22:33 +0200

    Improved ipod updater

--- ipodupdate.py
+++ ipodupdate.py
@@ -1,12 +1,39 @@
 #!/usr/bin/python
 import gpod
-ipodpath = raw_input('iPod Mountpoint: ')
+import pickle
+import sys
+sys.path.append('modules/')
+import datastruct
+import os
+
+ipod = raw_input('iPod Mountpoint: ')
 print 'Opening your iPod\'s database...'
-itdb = gpod.itdb_parse(ipodpath, None)
+itdb = gpod.itdb_parse(ipod, None)
+
+print 'Opening lyrics.db...'
+f = open('lyrics.db','r')
+data = pickle.load(f)
+f.close()
+
+print 'Collecting filenames in lyrics.db...'
+filelist = []
+for item in data:
+    api = datastruct.Structure(item)
+    filename = api.Filename()
+    lyric = api.Lyric()
+    if lyric != None and lyric != 'Error':
+        filelist.append(filename)
 
 print 'Alter values...'
 for track in gpod.sw_get_tracks(itdb):
-    track.lyrics_flag=1
+    name = track.ipod_path
+    name = name.replace(':', '/')
     
-print 'Saving in local file itunes.db...'
-gpod.itdb_write_file(itdb, 'itunes.db', None)
+    filename = ipod + name
+        
+    if filename in filelist:
+        print filename
+        track.lyrics_flag=1
+    
+print 'Saving in local file iTunesDB'
+gpod.itdb_write_file(itdb, 'iTunesDB', None)
