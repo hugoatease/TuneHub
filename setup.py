1e569ec3fe0cf709343721132b4f60d88077c70d
Hugo Caille <hugo@caille.tk>
2011-09-18 14:51:47 +0000

    Script to install now points on the .py estension

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 
 from distutils.core import setup
 
-setup(name='TuneHub', version='0.2', description='A tool for fetching lyrics on Internet and manage them', author='Hugo Caille', author_email='hugo@gkz.fr.nf', url='http://www.tunehub.tk', license='GNU GPL v.3', packages=['lyriclib','tunehubcore', 'progressbar', 'xmltodict'], package_dir={'progressbar': 'lib/', 'xmltodict': 'lib/'}, scripts=['tunehub'])
+setup(name='TuneHub', version='0.2', description='A tool for fetching lyrics on Internet and manage them', author='Hugo Caille', author_email='hugo@gkz.fr.nf', url='http://www.tunehub.tk', license='GNU GPL v.3', packages=['lyriclib','tunehubcore', 'progressbar', 'xmltodict'], package_dir={'progressbar': 'lib/', 'xmltodict': 'lib/'}, scripts=['tunehub.py'])
