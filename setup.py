d0920a51024d7c91756bc5595bd292a25b4fd8ee
Hugo Caille <hugo@caille.tk>
2011-09-20 20:36:22 +0200

    Patched progressbar and distutils bug

--- setup.py
+++ setup.py
@@ -13,9 +13,11 @@ if os.path.isfile('tunehub') == False:
         scriptname = 'tunehub'
     except:
         pass
-        
+else:
+	scriptname = 'tunehub'        
     
 
 from distutils.core import setup
 
-setup(name='TuneHub', version='0.2', description='A tool for fetching lyrics on Internet and manage them', author='Hugo Caille', author_email='hugo@gkz.fr.nf', url='http://www.tunehub.tk', license='GNU GPL v.3', packages=['lyriclib','tunehubcore', 'progressbar', 'xmltodict'], package_dir={'progressbar': 'lib/', 'xmltodict': 'lib/'}, scripts=[scriptname])
+setup(name='TuneHub', version='0.2', description='A tool for fetching lyrics on Internet and manage them', author='Hugo Caille', author_email='hugo@gkz.fr.nf', url='http://www.tunehub.tk', license='GNU GPL v.3', packages=['lyriclib','tunehubcore'], py_modules=['progressbar'], scripts=[scriptname])
+
